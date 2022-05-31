4769dc7d0b2f23214c18115210fc98059ef5808e
Aditya Khot <adityakhot6838@gmail.com>
2022-06-01 00:56:13 +0530

    navigation drawer ui

--- src/app/components/navigation-drawer/navigation-drawer.component.ts
+++ src/app/components/navigation-drawer/navigation-drawer.component.ts
@@ -7,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationDrawerComponent implements OnInit {
 
-  constructor() { }
+  furnitureArray: string[];
+ 
+
+  constructor() {
+    this.furnitureArray = [
+      "Sofa Chairs",
+      "Dining Tables",
+      "Mattresses",
+      "Kids Furnishings",
+      "Entertainment Units",
+      "Coffee Table",
+      "Wardrobes",
+      "Reclinears",
+      "Seating",
+      "Study Tables",
+      "Kids Storage",
+      "Bookshelves",
+      "Shoe Racks",
+      "Essentials",
+      "Wardrobes",
+      "Other"
+    ]
+
+
+  }
 
   ngOnInit(): void {
   }
