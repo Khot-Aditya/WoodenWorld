de3aeba23f016468ad7b9903ee954d09df0eadda
Aditya Khot <adityakhot6838@gmail.com>
2022-06-10 12:10:47 +0530

    explore page ui changes

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -12,4 +12,18 @@ export class ExploreComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getBestSellers() {
+    return [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  }
 }
